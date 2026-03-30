39d67ca46e0e4cbe5a1e1a368646cbb01d63e609
Abdul Quyoom <abdulquyoom402@gmail.com>
2026-03-30 17:45:24 +0500

    fixed another css issue

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'index.css'
+import './index.css'
 import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import OrderForm from './components/OrderForm'
